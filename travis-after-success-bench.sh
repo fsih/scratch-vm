74f3e197d71767b455fe0b89dd49a4d6957fc0f2
DD Liu <liudi@media.mit.edu>
2017-05-17 17:44:49 -0400

    add travis after success script

---
+++ travis-after-success-bench.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "deploy" ]; then
+    REMOTE_URL="$(git config --get remote.origin.url)";
+    # Clone the repository fresh..for some reason checking out master fails
+    # from a normal PR build's provided directory
+    cd ${TRAVIS_BUILD_DIR}/.. && \
+    git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" && \
+    cd  "${TRAVIS_REPO_SLUG}-bench" && \
+    # Bench master
+    git checkout develop && \
+    echo hello > hellofile && \
+    # Bench variable
+    git checkout ${TRAVIS_COMMIT} && \
+    echo bye > byefile && \
+    cat hellofile && \
+    cat byefile
+fi
